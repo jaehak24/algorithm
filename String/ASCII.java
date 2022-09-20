fce7e3cb1832c6c42b10d9e7331d8b26929e96ca
Jaehak Lee <65396939+jaehak24@users.noreply.github.com>
2022-09-20 09:29:43 +0900

    Starting Sting Problem

---
+++ String/ASCII.java
@@ -0,0 +1,14 @@
+package String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ASCII {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        char Word=(char)br.read();
+        System.out.println((int)Word);
+
+    }
+}
