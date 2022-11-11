7db08c92c4470755ed4f9ca49ced26a2eb410a09
jaehak24 <65396939+jaehak24@users.noreply.github.com>
2022-11-11 22:17:24 +0900

    Backtracking practice 2

--- BackTracking/NM_2.java
+++ BackTracking/NM_2.java
@@ -3,5 +3,12 @@ package BackTracking;
 public class NM_2 {
     public static void main(String[] args) {
 
+        int num1;
+        int num2;
+    }
+
+    public static int dfs(int num){
+
+        return 0;
     }
 }
