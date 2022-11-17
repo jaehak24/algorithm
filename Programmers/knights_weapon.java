90bc1840cd4cb2bcf099d89c1d1a7893949380f9
jaehak24 <65396939+jaehak24@users.noreply.github.com>
2022-11-17 22:31:43 +0900

    programmers 1

--- Programmers/knights_weapon.java
+++ Programmers/knights_weapon.java
@@ -1,21 +1,46 @@
 package Programmers;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class knights_weapon {
 
-    public int solution(int number, int limit, int power){
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        //number limit power 입력 예정
+        int num=Integer.parseInt(st.nextToken());
+        int limit=Integer.parseInt(st.nextToken());
+        int power=Integer.parseInt(st.nextToken());
+        System.out.println(solution(num,limit,power));
 
-        public int solution(int number, int limit, int power) {
-            int answer = 0;
 
-            for(int i=1; i<=number;i++){
-                int count=0;
-                for(int j=1;j<=i;j++){
-                    if(i%j==0) count++;
-                }
-                if(count>limit)answer+=power;
-                else answer+=count;
-            }
-            return answer;
+
+    }
+
+    public static int solution(int number, int limit, int power) {
+        int answer = 0;
+
+        for(int i=1; i<=number;i++){
+            int count=getNum(i);
+
+            if(count<=limit) answer+=count;
+            else answer+=power;
         }
+        return answer;
+    }
+
+    static int getNum(int num){
+
+        int cnt=0;
+        for (int i=1; i*i<=num;i++){
+            if(num%i==0) {
+                cnt++;
+                if(i*i<num) cnt++;
+            }
+        }
+        return cnt;
     }
 }
