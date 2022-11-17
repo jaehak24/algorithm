74ef230f7cccc1338e8b5cdb503291c9e818e05b
jaehak24 <65396939+jaehak24@users.noreply.github.com>
2022-11-17 21:52:54 +0900

    programmers 1

---
+++ DynamicProgramming/fibonacci.java
@@ -0,0 +1,36 @@
+package DynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class fibonacci {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        int num=Integer.parseInt(br.readLine());
+        System.out.println(fib(num)+" "+Dynamic_fib(num));
+
+    }
+
+
+    public static int fib(int num){
+        int result=0;
+        if(num==1||num==2) return 1;
+        else return fib(num-1)+fib(num-2);
+
+    }
+
+    public static int Dynamic_fib(int num){
+        int[] fib=new int[num];
+        int count=0;
+        fib[0]=fib[1]=1;
+        for (int i=2;i<num;i++){
+            fib[i]=fib[i-1]+fib[i-2];
+            count++;
+        }
+        return count;
+    }
+
+}
---
+++ Programmers/knights_weapon.java
@@ -0,0 +1,21 @@
+package Programmers;
+
+public class knights_weapon {
+
+    public int solution(int number, int limit, int power){
+
+        public int solution(int number, int limit, int power) {
+            int answer = 0;
+
+            for(int i=1; i<=number;i++){
+                int count=0;
+                for(int j=1;j<=i;j++){
+                    if(i%j==0) count++;
+                }
+                if(count>limit)answer+=power;
+                else answer+=count;
+            }
+            return answer;
+        }
+    }
+}
